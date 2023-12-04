48487af2f2825e1148b73cfa32fa9e9cb76d1e3f
stephane DrSmoke <stephanedrsmokecbd@gmail.com>
2023-12-04 18:53:45 +0100

    Ajout du calcul de promo

--- controller/admin.js
+++ controller/admin.js
@@ -63,7 +63,6 @@ exports.userPromotion = (req, res, next) => {
         if (error) {
             return res.status(501).json({message: `Une erreur est survenue: ${error}`});
         }
-        console.log(result)
 
         jwt.verify(req.body.token_utilisateur, "MY_SECRET_TOKEN", (err, decode) => {
             if (err) { return res.status(401).json({ message: 'jwt not active', IsTrue: false }) }
@@ -85,25 +84,53 @@ exports.userPromotion = (req, res, next) => {
                         }
                     }
                     // $sqlPromo = `UPDATE utilisateur SET promo = 'nouvelle valeur' WHERE condition`
-                    console.log(aTroisChiffre(prix))
                     // console.log(troisiemeChiffre(prix))
                     // console.log(resultUSers);
                     // const promoUtilisateur = resultUSers.map(obj => { return obj.promo });
                     // const uuid_shop_promo = resultUsers.map(obj => { return obj.uuid_shop_promo }); 
                     const villeShop = result.map(obj => { return obj.ville })[0];
                     $SelectPromo = `SELECT * FROM promo_utilisateur WHERE uuid_user = ${mysqlConnection.escape(decode.uuid)} AND uuid_admin = ${mysqlConnection.escape(req.auth.userId)}`;
-                    console.log($SelectPromo)
+
                     mysqlConnection.query($SelectPromo, (errorPromo, resultPromo, fieldsPromo) => {
                         if (errorPromo) {
                             return res.status(501).json({message: `Une erreur est survenue de promo: ${error}`});
                         }
                         else {
-                            if (resultPromo.length === 0) {
+                            if (resultPromo.length == 0) {
                                 uuidPromo = mysqlConnection.escape(uuidv4());
                                 $insertPromo = `INSERT INTO promo_utilisateur(uuid_promo, uuid_user, uuid_admin, prix, ville_shop) VALUES(${uuidPromo}, ${mysqlConnection.escape(decode.uuid)}, ${mysqlConnection.escape(req.auth.userId)}, ${req.body.prix}, ${villeShop})`;
                                 mysqlConnection.query($insertPromo, (errorInsertPromo, resultInsertPromo, fieldsInsertPromo) => {
-                                    
+                                    if (error) {
+                                        return res.status(501).json({message: `Une erreur est survenue: ${error}`});
+                                    }
+                                    return res.status(201).json({message: 'La promo a bien été ajouter'});
                                 })
+                            } else {
+                                const ResultPrix = resultPromo.map(obj => obj.prix)[0];
+
+                                const nouveauPrix = parseFloat(req.body.prix);
+                                if (!isNaN(nouveauPrix)) {
+                                    const resultTotal = ResultPrix + nouveauPrix;
+                                    console.log(resultTotal);
+
+                                    const $SqlUpdatePromo = `UPDATE promo_utilisateur SET prix = ? WHERE uuid_user = ? AND uuid_admin = ?`;
+                                    const values = [resultTotal, decode.uuid, req.auth.userId];
+
+                                    mysqlConnection.query($SqlUpdatePromo, values, (errorUpdate, resultUpdate, fieldsUpdate) => {
+                                        if (errorUpdate) {
+                                            res.status(201).json({ message: 'Une erreur est survenue' });
+                                        } else {
+                                            if (aTroisChiffre(resultTotal)) {
+                                                const troisieme = troisiemeChiffre(resultTotal);
+                                                res.status(201).json({ message: `Une promo de: ${troisieme} est disponible` });
+                                            } else {
+                                                res.status(201).json({ message: 'La promo a bien été enregistrée' });
+                                            }
+                                        }
+                                    });
+                                } else {
+                                    res.status(400).json({ message: 'La valeur de prix est invalide' });
+                                }
                             }
                         }
                     })
